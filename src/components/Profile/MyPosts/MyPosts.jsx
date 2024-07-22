74bb9d6d5254a78d0439ab202b7f50a318ee221f
bogdan <buffon-95@mail.ru>
2024-07-21 17:01:03 -0700

    adding reducers. refactoring STATEJS

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Posts/Post';
-import { addPostActionCreator } from '../../../redux/state';
-import { updateNewPostTextActionCreator } from '../../../redux/state';
+import { updateNewPostTextActionCreator, addPostActionCreator } from '../../../redux/profile-reducer';
 
 
 const MyPosts = (props) => {
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,15 +1,13 @@
-const ADD_POST = 'ADD-POST';
+import dialogsReducer from "./dialogs-reducer";
+import profileReducer from "./profile-reducer";
+import sidebarReducer from "./sidebar-reducer copy";
 
-const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
-
-const UPDATE_NEW_MESSAGE_BODY = 'UPDATE_NEW_MESSAGE_BODY';
-const SEND_MESSAGE = 'SEND_MESSAGE';
 
 let store = {
     _state: {
 
         dialogsPage: {
-    
+
             dialogs: [
                 { id: 1, name: 'Bogdan' },
                 { id: 2, name: 'Veronika' },
@@ -24,21 +22,22 @@ let store = {
             ],
             newMessageBody: ""
         },
-     
+
         profilePage: {
             posts: [
                 { id: 1, message: 'Hi how are you ?', likesCount: 12 },
                 { id: 2, message: 'It is my first post', likesCount: 32 }
             ],
             newPostText: 'trying to put first post to state'
-        }
+        },
+        sidebar: {}
     },
-    
-    getState(){
+
+    getState() {
         return this._state;
     },
 
-    
+
     _callSubscriber() {
         console.log('state changed');
     },
@@ -48,57 +47,15 @@ let store = {
         this._callSubscriber = observer;
     },
 
-    dispatch(action) {//наш state меняется через dispatch actions
-        if(action.type === ADD_POST){
-            let newPost = {
-                id: 5,
-                message: this._state.profilePage.newPostText,
-                likesCount: 0
-            };
-            
-            this._state.profilePage.posts.push(newPost); // в коннец массива добавляет новый жлемент 
-            this._state.profilePage.newPostText = '';//делаю пустую строку после добавления
-            this._callSubscriber(this._state);
-        }else if (action.type === UPDATE_NEW_POST_TEXT) {
-            this._state.profilePage.newPostText = action.newText; // обновляем данный введенные в текстбар
-        this._callSubscriber(this._state);//перерисовывваем все дерево
-        }else if (action.type === UPDATE_NEW_MESSAGE_BODY) { //пользователь вводи сообщения 
-            this._state.dialogsPage.newMessageBody = action.body;
-            this._callSubscriber(this._state);
-        }else if (action.type === SEND_MESSAGE) { //
-            let body = this._state.dialogsPage.newMessageBody;
-            this._state.dialogsPage.newMessageBody ='';
-            this._state.dialogsPage.messages.push({ id: 6, message: body}); // записываю в новые сообщения то что написал пользователь
-            this._callSubscriber(this._state);
-        }
+    dispatch(action) {//наш state меняется через dispatch actions. action это обьект у которого как минимум есть свойство TYPE
+
+        this._state.profilePage = profileReducer(this._state.profilePage, action);
+        this._state.dialogsPage = dialogsReducer(this._state.dialogsPage, action);
+        this._state.sidebar = sidebarReducer(this._state.sidebar, action);
+
+        this._callSubscriber(this._state);
     }
-  
-}
 
-export const addPostActionCreator = () => {
-    return {
-        type: ADD_POST
-    }
-}
-
-export const updateNewPostTextActionCreator = (text) => {
-    return {
-        type: UPDATE_NEW_POST_TEXT, newText: text
-    }
-}
-
-
-
-export const sendMessageCreator = () => {
-    return {
-        type: SEND_MESSAGE
-    }
-}
-
-export const updateNewMessageBodyCreator = (body) => {
-    return {
-        type: UPDATE_NEW_MESSAGE_BODY, body: body //этот body идет от этого условия (action.type === UPDATE_NEW_MESSAGE_BODY)
-    }
 }
 
 export default store;
